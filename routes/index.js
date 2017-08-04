2117c65fde737fb70a0aad92fe7e6d010d6be771
TeeKenLau <dknight1938@hotmail.com>
2017-08-04 13:44:02 +1000

    Socket Connection is Established

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  res.render('index', { title: 'Express' });
+  res.io.sockets.on('connection',function(socket){
+      //Establishment of connection
+      console.log("The socket connection is established");
+
+      socket.emit('connect',{
+          "socket_message" : "You have now connected to chat room"
+      });
+      socket.emit('connectToUser',"You have now connected to the chat room");
+
+      socket.on('sendMessage',function(data){
+         console.log(data);
+      });
+
+      socket.on('disconnect',function(data){
+          console.log("User is disconnected");
+      });
+  });
+
+});
+
+
+router.get('/chat',function(req, res, next) {
+    res.render('chat',{title: 'Express'});
+});
+
+module.exports = router;
