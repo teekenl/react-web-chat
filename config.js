10a09fe42cfab57155dbfba6119103eb311adab8
teekenl <dknight1938@hotmail.com>
2017-10-23 19:42:12 +1000

    allowedCORSOrigins added in config js

--- config.js
+++ config.js
@@ -3,5 +3,6 @@
 module.exports = {
     serverPort: 7331,
     sessionCookieKey: "connect.sid",
-    sessionSecret: "SESIORsecret"
+    sessionSecret: "SESIORsecret",
+    allowedCORSOrigins: "http://127.0.0.1:9000"
 };
